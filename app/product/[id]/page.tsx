1c428d93b0dc6509fe3c7067713b4be76aaf6719
3narinder <narinderd9@gmail.com>
2025-10-05 15:42:31 +0530

    loading- oprimization

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,4 +1,3 @@
-// app/product/[id]/page.tsx
 import { notFound } from "next/navigation";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
@@ -12,11 +11,12 @@ import ProductImageGallery from "@/app/components/ProductImageGallery";
 import Breadcrumb from "@/app/components/BreadCrumb";
 
 interface ProductPageProps {
-  params?: { id?: string };
+  params: Promise<{ id: string }>;
 }
 
-const ProductPage = ({ params }: ProductPageProps) => {
-  const product = products?.find((p) => p?.id?.toString() === params?.id);
+export default async function ProductPage({ params }: ProductPageProps) {
+  const { id } = await params;
+  const product = products?.find((p) => p?.id?.toString() === id);
 
   if (!product) return notFound();
 
@@ -24,27 +24,18 @@ const ProductPage = ({ params }: ProductPageProps) => {
     <div className="lg:px-32 py-8 bg-neutral-2">
       <Breadcrumb productName={product.name} />
       <div className="mt-12 flex lg:flex-row lg:justify-start flex-col lg:gap-14 gap-12 lg:ml-0 mx-4">
-        {/* product image gallery */}
         <ProductImageGallery images={product.image} alt={product.name} />
-
-        {/* Product detail */}
         <div className="flex flex-col">
-          {/* Product name */}
           <h1 className="text-display-4 font-semibold uppercase text-warm-black tracking-wide">
             {product?.name}
           </h1>
-
-          {/* star rating and stock */}
           <div className="flex items-center mt-2">
             <StarRating rating={4} size={16} />
-
             <div className="text-display-2 text-neutral-7 ml-4">
               (1256 Reviews)
             </div>
-
             <div className="text-display-2 ml-12 flex">
               <span className="text-neutral-7">Stock :</span>
-
               <span
                 className={`ml-1 ${
                   product.availability === "in stock"
@@ -56,8 +47,6 @@ const ProductPage = ({ params }: ProductPageProps) => {
               </span>
             </div>
           </div>
-
-          {/* price */}
           <div className="flex gap-3 mt-2">
             <span className="text-display-6 text-neutral-6 tracking-wide">
               $500
@@ -66,16 +55,11 @@ const ProductPage = ({ params }: ProductPageProps) => {
               ${product?.price}
             </span>
           </div>
-
-          {/* product action */}
           <ProductActions product={product} />
-
-          {/* social media  */}
           <div className="lg:mt-12 mt-8">
             <div className="text-display-3 font-semibold text-warm-black">
               Share this:
             </div>
-
             <div className="flex items-center gap-3 text-light-brown mt-2">
               <FaFacebookF className="text-lg cursor-pointer" />
               <FaTwitter className="text-lg cursor-pointer" />
@@ -83,19 +67,15 @@ const ProductPage = ({ params }: ProductPageProps) => {
               <FaLinkedinIn className="text-lg cursor-pointer" />
             </div>
           </div>
-
           <div className="lg:mt-12 mt-8">
             <ProductAccordion />
           </div>
         </div>
       </div>
-
       <div className="lg:py-20 py-12">
         <SectionHeading alignStart text="Similar items" />
         <ProductList grid4 limit={8} />
       </div>
     </div>
   );
-};
-
-export default ProductPage;
+}
